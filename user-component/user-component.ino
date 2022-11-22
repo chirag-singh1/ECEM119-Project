46a8f32183c8a81638bd144fd686a40be12ff2c3
Chirag Singh <chirag.singh3489@gmail.com>
2022-11-22 13:18:51 -0800

    yes

--- user-component/user-component.ino
+++ user-component/user-component.ino
@@ -1,52 +1,96 @@
 #include <Arduino_LSM6DS3.h>
 #include <ArduinoBLE.h>
+#include <WiFiNINA_Generic.h>
+
+const int BUFSIZE = 1000;
+const int FREQUENCY = 200;
 
 float ax, ay, az;
-int t;
+float av[BUFSIZE];
+int t[BUFSIZE];
+int ind;
 
-// Bluetooth variables
-BLEService calibrationServiceService("180B"); // BLE authentication Service
-BLEByteCharacteristic switchCharacteristic("2A58", BLERead | BLEWrite); // BLE authentication characteristic
+// Network info
+char ssid[] = "ARDUINO_NET";
+char pass[] = "ARDUINO_PASS";
+
+// Wifi locals
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  IMU.begin();
-
-  // Initialize BLE
-  if (!BLE.begin()) {
-    Serial.println("BLE Init Failed");
-    while (1);
+  // Create Arduino Access point
+  while (status != WL_AP_LISTENING) {
+    Serial.print("Attempting to create network: ");
+    Serial.println(ssid);
+    status = WiFi.beginAP(ssid, pass);
+    delay(10000);
   }
-  BLE.setLocalName("KnowTouch - User Component");
-  BLE.setAdvertisedService(calibrationServiceService);
-  calibrationServiceService.addCharacteristic(switchCharacteristic);
-  BLE.addService(calibrationServiceService);
-  switchCharacteristic.writeValue(1);
-  BLE.advertise();
-  Serial.println("BLE Init complete");
+
+  // Print network info after connecting
+  Serial.println("Connected to the network");
+  Serial.println("---------------------------------------");
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  server.begin();
+  IMU.begin();
 }
 
 void loop() {
-  // Listen for user component
-  BLEDevice central = BLE.central();
 
-  // User component connected, perform basic logic
-  while (central && central.connected()) {
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("New client");
 
+    // Write IMU data to HTTP request to respond to client
+    while (client.connected())
+    {
+      delayMicroseconds(10);
+      if (client.available()) {
+        client.println("HTTP/1.1 200 OK");
+        client.println("Content-type:application/json");
+        client.println();
+        client.println("{");
+        client.print("\"t\":[");
+        for (int i = 0; i < ind - 1; i++) {
+          client.print(t[i]);
+          client.print(",");
+        }
+        client.print(t[ind-1]);
+        client.println("],");
+        client.print("\"av\":[");
+        for (int i = 0; i < ind - 1; i++) {
+          client.print(av[i]);
+          client.print(",");
+        }
+        client.print(av[ind-1]);
+        client.println("]");
+        client.println("}");
+        client.println();
+        break;
+      }
+    }
+
+    // Wait for a little after each client
+    delay(50);
+    client.stop();
+
+    ind = 0;
   }
 
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(ax, ay, az);
-    t = millis();
+    t[ind] = millis();
+    av[ind] = sqrt(ax*ax+ay*ay+az*az);
+    Serial.println(t[ind]);
+    Serial.println(av[ind]);
+    ind = (ind + 1) % BUFSIZE;
   }
 
-  Serial.print(ax);
-  Serial.print(",");
-  Serial.print(ay);
-  Serial.print(",");
-  Serial.print(az);
-  Serial.print(",");
-  Serial.println(t);
+  delay(1500 / FREQUENCY);
 }
--- user-component/user-component.py
+++ user-component/user-component.py
@@ -1,10 +1,12 @@
-import serial
 import numpy as np
 from threading import Thread
 from scipy.fftpack import fft
+import requests
+import time
 
 BUFSIZE = 1000
 FREQUENCY = 200.0
+REFRESH_SPEED = 3.0
 
 def find_border(t, n, dt, start_time):
     for i in range(len(t)):
@@ -19,46 +21,36 @@ def generate_interpolated_data(data, t):
     dt = 1000.0 / FREQUENCY
     n = 1
 
-    t = np.array([0.0])
+    ot = np.array([0.0])
     out = np.array([data[0]])
     while n*dt + start_time < end_time:
         prev, curr = find_border(t, n, dt, start_time)
         ct = start_time + n * dt
         if curr < BUFSIZE:
-            t = np.append(t, [n*dt])
+            ot = np.append(t, [n*dt])
             out = np.append(out, [data[prev] * (1-((ct - t[prev]) / (t[curr] - t[prev]))
                                                 ) + data[curr] * (1-((t[curr] - ct) / (t[curr] - t[prev])))])
             n += 1
 
-    return out, t
+    return out, ot
 
 
 class ProcessAuthenticationTask(Thread):
     def run(self):
-        ser = serial.Serial('/dev/ttyACM0')
-        ser.baudrate = 9600
-        ax = np.zeros((BUFSIZE))
-        ay = np.zeros((BUFSIZE))
-        az = np.zeros((BUFSIZE))
-        t = np.zeros((BUFSIZE))
-
         while True:
-            for i in range(BUFSIZE):
-                data_line = ser.readline().decode('utf-8').strip().split(',')
-                if len(data_line) == 4:
-                    ax[i] = float(data_line[0])
-                    ay[i] = float(data_line[1])
-                    az[i] = float(data_line[2])
-                    t[i] = int(data_line[3])
+            curr_req_start_time = time.time()
+            try:
+                x = requests.get('http://192.168.4.1', timeout=8)
 
-            axint, tint = generate_interpolated_data(ax, t)
-            ayint, tint = generate_interpolated_data(ax, t)
-            azint, tint = generate_interpolated_data(ax, t)
-            print(axint)
+                av, t = generate_interpolated_data(x.json()['av'], x.json()['t'])
+                print(t)
+                print(av)
+                if time.time() - curr_req_start_time < REFRESH_SPEED * 1000:
+                    time.sleep(REFRESH_SPEED - (time.time() - curr_req_start_time) / 1000)
+            except:
+                print("exception!!!!")
 
 
 if __name__ == '__main__':
-    # a = ReadDataTask()
     b = ProcessAuthenticationTask()
-    # a.start()
     b.start()
